4842782d78a7fdef7a0b522cde6c4171a656c57d
Gabriel Guerra <gfranklings@gmail.com>
2021-08-14 23:48:41 -0400

    install.sh: errors fixes

--- bin/install.sh
+++ bin/install.sh
@@ -44,27 +44,27 @@ if [ -f /usr/bin/copier ]; then
     fi
 fi
 
-mkdir .copier-install-tmp
+mkdir -p .copier-install-tmp
 cd .copier-install-tmp
 
 if [ -f .copier-install-error ]; then
     rm .copier-install-error
 fi
 
-printf "Downloading the copier source code..."
+printf "Downloading the copier source code... "
 git clone https://github.com/AlphaTechnolog/copier.git copier-src > /dev/null 2> .copier-install-error
 status_code=$?
 
-if ! status_code; then
+if [[ $status_code != 0 ]]; then
     cd ..
     rm -rf .copier-install-tmp
     error "FAILURE... An error was ocurred when I download copier: " $(cat .copier-install-error)
 fi
 
-printf "OK.\nMoving the source code to /opt/copier"
+printf "OK.\nMoving the source code to /opt/copier... "
 sudo mv ./copier-src /opt/copier
 sudo chmod -R 777 /opt/copier /opt/copier/*
-printf "OK.\nCreating the symbolic link to execute it on your \$PATH..."
+printf "OK.\nCreating the symbolic link to execute it on your \$PATH... "
 sudo ln -s /opt/copier/copier.py /usr/bin/copier
 sudo chmod -R 777 /usr/bin/copier
 echo "OK. type \"copier\" to start!"
